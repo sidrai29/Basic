cce096bba9c6ddfd6575d982d1b2ca55aca357b1
sidraibtech <73195623+sidraibtech@users.noreply.github.com>
2021-10-01 18:49:25 -0700

    Update FibonacciUpToNTerms.c
    updated

--- FibonacciUpToNTerms.c
+++ FibonacciUpToNTerms.c
@@ -2,15 +2,16 @@
 void main() 
 {
     int i, n, a= 0, b= 1, sumab;
-    printf(" Enter the number to find its Fibonacci Series up to n terms: ");
+    printf(" Enter the number");
     scanf("%d", &n);
     printf("Fibonacci Series up to n terms:");
-
-    for (i = 0; i <= n; i++) 
+    
+    while(i<=n)
     {
         printf("%d ",a);
         sumab = a + b;
         a = b;
         b = sumab;
+        i++;
     }
 }
