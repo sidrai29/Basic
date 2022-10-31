026382580ee19e515c236d56a0754486f50eb930
Siddhika18 <72340821+Siddhika18@users.noreply.github.com>
2022-10-31 09:25:38 +0530

    Update strings.c
    added shark
    HACKTOBERFEST

--- strings.c
+++ strings.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    char str[]={'T','I','T','L','I','\0'};
+    char str[]={'S','H','A','R','K','\0'};
     char *ptr=str;
     while(*ptr!='0'){
         printf("%c",*ptr);
